9883d657041cc77ff98d6c9367f716b26bf20af4
fup1990 <fup1990@163.com>
2017-07-28 11:06:28 +0800

    继承PropertyPlaceholderConfigurer

---
+++ easyconfig-agent/src/main/java/com/gome/fup/easyconfig/config/EasyPropertyPlaceholderConfigurer.java
@@ -0,0 +1,54 @@
+package com.gome.fup.easyconfig.config;
+
+import com.gome.fup.easyconfig.util.EasyConfigUtil;
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
+import org.springframework.beans.factory.config.PropertyPlaceholderConfigurer;
+
+import java.util.Map;
+import java.util.Properties;
+
+/**
+ * Created by fupeng-ds on 2017/7/28.
+ */
+public class EasyPropertyPlaceholderConfigurer extends PropertyPlaceholderConfigurer {
+
+    public EasyConfigUtil configUtil;
+
+    public Long projectId;
+
+    public String groupName;
+
+    @Override
+    protected void processProperties(ConfigurableListableBeanFactory beanFactoryToProcess, Properties props) throws BeansException {
+        super.processProperties(beanFactoryToProcess, props);
+        Properties config = configUtil.getConfig(projectId, groupName);
+        for (Map.Entry<Object, Object> entry : config.entrySet()) {
+            props.put(entry.getKey(), entry.getValue());
+        }
+    }
+
+    public Long getProjectId() {
+        return projectId;
+    }
+
+    public void setProjectId(Long projectId) {
+        this.projectId = projectId;
+    }
+
+    public String getGroupName() {
+        return groupName;
+    }
+
+    public void setGroupName(String groupName) {
+        this.groupName = groupName;
+    }
+
+    public EasyConfigUtil getConfigUtil() {
+        return configUtil;
+    }
+
+    public void setConfigUtil(EasyConfigUtil configUtil) {
+        this.configUtil = configUtil;
+    }
+}
