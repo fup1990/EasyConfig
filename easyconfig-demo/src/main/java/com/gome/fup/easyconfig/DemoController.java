db30ace1c889e09cb3e81df348fcaf74478f45ba
fup1990 <fup1990@163.com>
2017-08-01 16:23:48 +0800

    测试优化

---
+++ easyconfig-demo/src/main/java/com/gome/fup/easyconfig/DemoController.java
@@ -0,0 +1,17 @@
+package com.gome.fup.easyconfig;
+
+/**
+ * Created by fupeng-ds on 2017/8/1.
+ */
+public class DemoController {
+
+    private String str;
+
+    public String getStr() {
+        return str;
+    }
+
+    public void setStr(String str) {
+        this.str = str;
+    }
+}
--- easyconfig-demo/src/main/java/com/gome/fup/easyconfig/Test.java
+++ easyconfig-demo/src/main/java/com/gome/fup/easyconfig/Test.java
@@ -11,10 +11,12 @@ import java.util.Properties;
  */
 public class Test {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         ApplicationContext context = new ClassPathXmlApplicationContext("spring-config.xml");
-        EasyConfigUtil util = context.getBean(EasyConfigUtil.class);
-        Properties p = util.getConfig("12", "test1");
-        System.out.println(p);
+        DemoController demo = context.getBean(DemoController.class);
+        System.out.println("before change:" + demo.getStr());
+        //通过server修改配置信息
+        Thread.sleep(5000l);
+        System.out.println("after change:" + demo.getStr());
     }
 }
